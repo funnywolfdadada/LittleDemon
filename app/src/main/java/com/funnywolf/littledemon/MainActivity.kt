01602505ab90d04cd747dc4acfdc45d8a73f01f9
funnywolf <1748787587@qq.com>
2019-08-31 20:51:26 +0800

    add HostFragment

--- app/src/main/java/com/funnywolf/littledemon/MainActivity.kt
+++ app/src/main/java/com/funnywolf/littledemon/MainActivity.kt
@@ -1,119 +1,49 @@
 package com.funnywolf.littledemon
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import com.funnywolf.littledemon.fragments.*
-import com.funnywolf.littledemon.test.InnerStaticClassTest
-import com.funnywolf.littledemon.test.testCoroutine
-import com.funnywolf.littledemon.utils.LiveDataBus
-import com.funnywolf.littledemon.utils.MainHandlerUtils
-import io.reactivex.Observable
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
+import com.funnywolf.littledemon.live.BaseLiveObservable
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        const val TAG = "MainActivity"
-    }
-
-    private val fragmentData = LiveDataBus.get().getData(Fragment::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        LiveDataBus.get().observe(Fragment::class.java, this, Observer {
-            openFragment(it ?: return@Observer)
-        })
-        initViews()
+        addFragment(FragmentIntent(HostFragment::class.java), false)
+        intentObserver.addObserver(this::startFragment, this, false)
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        // 测试并发
-        val stopTime = System.nanoTime() + 1000 * 1000 * 1000
-        repeat(10) {
-            Thread {
-                while (System.nanoTime() < stopTime) {}
-                Log.d(TAG, "$it: ${Thread.currentThread()} ${LiveDataBus.get(TAG).hashCode()}")
-            }.start()
-        }
-
-        // 测试 coroutine
-        testCoroutine()
-
-        // 测试静态内部类的初始化
-        InnerStaticClassTest.doSomethingSelf()
-        Observable.just(0)
-            .delay(1000, TimeUnit.MILLISECONDS)
-            .doOnNext {
-                InnerStaticClassTest.doSomethingWithInnerStaticClass()
-            }
-            .subscribe()
-
-        // 静态内部类的 Handler
-        MainHandlerUtils.post {
-            Toast.makeText(this, "MainHandlerUtils", Toast.LENGTH_SHORT).show();
-        }
+    private fun startFragment(intent: FragmentIntent?) {
+        addFragment(intent ?: return, true)
     }
 
-    private fun initViews() {
-        dragClose.setOnClickListener {
-//            fragmentData.postValue(DragCloseFragment())
-            LiveDataBus.get().post(Fragment::class.java, DragCloseFragment())
-        }
-
-        dragExtend.setOnClickListener {
-            fragmentData.postValue(DragExtendFragment())
-        }
-
-        nestScrollView.setOnClickListener {
-            fragmentData.postValue(NestScrollViewFragment())
-        }
-
-        constrainLayoutTest.setOnClickListener {
-            fragmentData.postValue(ConstraintLayoutTestFragment())
-        }
-
-        scrollAndDragExtend.setOnClickListener {
-            fragmentData.postValue(ScrollAndDragExtentFragment())
-        }
-
-        simpleList.setOnClickListener {
-            fragmentData.postValue(SimpleListFragment())
-        }
-
-        fragmentViewPager.setOnClickListener {
-            fragmentData.postValue(FragmentViewPagerFragment())
-        }
-
-        layoutTest.setOnClickListener {
-            fragmentData.postValue(LayoutTestFragment())
-        }
-
-        foldedText.setOnClickListener {
-            fragmentData.postValue(FoldedTextFragment())
-        }
-
-        drawerList.setOnClickListener {
-            fragmentData.postValue(DrawerListFragment())
-        }
-
-        popupWindowTest.setOnClickListener {
-            fragmentData.postValue(PopupWindowTestFragment())
-        }
-    }
-
-    private fun openFragment(fragment: Fragment) {
+    private fun addFragment(intent: FragmentIntent, toStack: Boolean?) {
         supportFragmentManager
             .beginTransaction()
-            .add(R.id.fragment_content, fragment)
-            .addToBackStack(null)
+            .replace(R.id.fragment_content, intent.fragmentClass.newInstance().apply {
+                arguments = intent.bundle
+            })
+            .apply {
+                if (toStack == true) {
+                    addToBackStack(intent.fragmentClass.name)
+                }
+            }
             .commit()
     }
+
+    data class FragmentIntent(val fragmentClass: Class<out Fragment>, val bundle: Bundle? = null)
+
+    companion object {
+
+        private val intentObserver = BaseLiveObservable<FragmentIntent>()
+
+        fun startFragment(fragmentClass: Class<out Fragment>, bundle: Bundle? = null) {
+            intentObserver.dispatchValue(FragmentIntent(fragmentClass, bundle))
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/funnywolf/littledemon/fragments/HostFragment.kt
@@ -0,0 +1,43 @@
+package com.funnywolf.littledemon.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.funnywolf.littledemon.MainActivity
+import com.funnywolf.littledemon.R
+import kotlinx.android.synthetic.main.fragment_layout_host.*
+
+class HostFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_layout_host, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        bind(dragClose, DragCloseFragment::class.java)
+        bind(dragExtend, DragExtendFragment::class.java)
+        bind(nestScrollView, NestScrollViewFragment::class.java)
+        bind(constrainLayoutTest, ConstraintLayoutTestFragment::class.java)
+        bind(scrollAndDragExtend, ScrollAndDragExtentFragment::class.java)
+        bind(simpleList, SimpleListFragment::class.java)
+        bind(fragmentViewPager, FragmentViewPagerFragment::class.java)
+        bind(layoutTest, LayoutTestFragment::class.java)
+        bind(foldedText, FoldedTextFragment::class.java)
+        bind(drawerList, DrawerListFragment::class.java)
+        bind(popupWindowTest, PopupWindowTestFragment::class.java)
+    }
+
+    private fun bind(v: View, fragmentClass: Class<out Fragment>) {
+        v.setOnClickListener {
+            MainActivity.startFragment(fragmentClass)
+        }
+    }
+
+}

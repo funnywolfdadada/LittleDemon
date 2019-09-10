94eb2ffbdf925bab21420634c13e4d298b01299a
zhaodongliang <zhaodongliang@zhihu.com>
2019-09-10 17:53:48 +0800

    完善 SimpleBottomSheetBehavior

--- app/src/main/java/com/funnywolf/littledemon/fragments/ViewDragHelperTestFragment.kt
+++ app/src/main/java/com/funnywolf/littledemon/fragments/ViewDragHelperTestFragment.kt
@@ -1,13 +1,16 @@
 package com.funnywolf.littledemon.fragments
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
 import androidx.customview.widget.ViewDragHelper
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
@@ -122,6 +125,12 @@ class SimpleBottomSheetBehavior : CoordinatorLayout.Behavior<View> {
     var dragViewId: Int = 0
     private var dragHelper: ViewDragHelper? = null
     private var parentRef: WeakReference<CoordinatorLayout>? = null
+    private var scrollableChildRef: WeakReference<View>? = null
+
+    /**
+     * for [onInterceptTouchEvent]
+     */
+    private var lastY: Float = 0F
 
     private val dragCallback = object: ViewDragHelper.Callback() {
         override fun tryCaptureView(child: View, pointerId: Int): Boolean {
@@ -136,12 +145,16 @@ class SimpleBottomSheetBehavior : CoordinatorLayout.Behavior<View> {
         override fun onViewReleased(releasedChild: View, xvel: Float, yvel: Float) {
             if (releasedChild.id != dragViewId) { return }
             val parent = parentRef?.get() ?: return
-            val finalTop = if (yvel > 0) {
-                parent.height - releasedChild.minimumHeight
-            } else {
-                0
+            val expandTop = 0
+            val collapseTop = parent.height - releasedChild.minimumHeight
+            val finalTop = when {
+                yvel >= 0 -> collapseTop
+                releasedChild.top > collapseTop -> collapseTop
+                else -> expandTop
             }
-            releasedChild.animate().y(finalTop.toFloat()).start()
+            if (dragHelper?.smoothSlideViewTo(releasedChild, releasedChild.left, finalTop) == true) {
+                ViewCompat.postOnAnimation(releasedChild, SettleRunnable(releasedChild))
+            }
         }
     }
 
@@ -155,6 +168,10 @@ class SimpleBottomSheetBehavior : CoordinatorLayout.Behavior<View> {
     ): Boolean {
         parentRef = WeakReference(parent)
         dragHelper = ViewDragHelper.create(parent, dragCallback)
+        val scrollableChild = findScrollableChild(child)
+        if (scrollableChild != null) {
+            scrollableChildRef = WeakReference(scrollableChild)
+        }
         return false
     }
 
@@ -170,27 +187,30 @@ class SimpleBottomSheetBehavior : CoordinatorLayout.Behavior<View> {
         parentRef = null
     }
 
-    private var lastY: Float = 0F
     override fun onInterceptTouchEvent(
         parent: CoordinatorLayout,
         child: View,
         ev: MotionEvent
     ): Boolean {
+        if (child.top > 0) {
+            return true
+        }
         when (ev.action) {
             MotionEvent.ACTION_DOWN -> {
                 lastY = ev.y
             }
             MotionEvent.ACTION_MOVE -> {
+                val scrollableChild = scrollableChildRef?.get() ?: return false
                 val dy = ev.y - lastY
                 lastY = ev.y
-                if (dy > 0 && !child.canScrollVertically(-dy.toInt())) {
+                if (dy > 0 && !scrollableChild.canScrollVertically(-dy.toInt())) {
                     ev.action = MotionEvent.ACTION_DOWN
                     dragHelper?.processTouchEvent(ev)
                     return true
                 }
             }
         }
-        return child.top != 0
+        return false
     }
 
     override fun onTouchEvent(
@@ -203,6 +223,29 @@ class SimpleBottomSheetBehavior : CoordinatorLayout.Behavior<View> {
         } else {
             dragHelper?.processTouchEvent(ev)
             true
+        }
+    }
+
+    private fun findScrollableChild(view: View): View? {
+        if (ViewCompat.isNestedScrollingEnabled(view)) {
+            return view
+        }
+        if (view is ViewGroup) {
+            for (i in 0 until view.childCount) {
+                val scrollableChild = findScrollableChild(view.getChildAt(i))
+                if (scrollableChild != null) {
+                    return scrollableChild
+                }
+            }
+        }
+        return null
+    }
+
+    private inner class SettleRunnable(val view: View): Runnable {
+        override fun run() {
+            if (dragHelper?.continueSettling(true) == true) {
+                ViewCompat.postOnAnimation(view, this)
+            }
         }
     }
 
